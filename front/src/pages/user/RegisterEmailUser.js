14cc8ac4be35cfd95928a561d1392ac12158f430
Jerry Andriam <jerryandriam@gmail.com>
2024-10-09 23:13:02 +0300

    dialog auth user

--- front/src/pages/user/RegisterEmailUser.js
+++ front/src/pages/user/RegisterEmailUser.js
@@ -6,17 +6,20 @@ import { InputText } from "primereact/inputtext"
 import { FloatLabel } from "primereact/floatlabel"
 import { Link } from "react-router-dom"
 import { Carousel } from "primereact/carousel"
+import { Dialog } from 'primereact/dialog'
 
 import NavigationMenu2 from "../../components/inc/NavigationMenu2"
 import { useLanguage } from "../../context/LanguageContext"
 
 import gare from '../../images/assets/gare.webp'
+import emailVerify from '../../images/assets/emailVerif.png'
 
 const RegisterEmailUser = () => {
     const { t } = useLanguage()
 
+    const [loading, setLoading] = useState(false)
     const [emailvalue, emailSetValue] = useState('')
-
+    const [emailInterface, setEmailInterface] = useState(false)
 
     const textIntros = [
         { id: 1, text: t('textIntro1') },
@@ -30,8 +33,13 @@ const RegisterEmailUser = () => {
         </div>
     )
 
-    const verifyEmail = () => {
-
+    const verifyEmail = (e) => {
+        setLoading(true)
+        setTimeout(() => {
+            e.preventDefault()
+            setLoading(false)
+            setEmailInterface(true)
+        }, 1500)
     }
 
     return (
@@ -58,7 +66,16 @@ const RegisterEmailUser = () => {
                         </FloatLabel>
                     </div>
 
-                    <Button label={t('submit')} className="bg-slate-900 text-white mt-10 font-poppins border border-none outline outline-none rounded py-2 px-36 max-[500px]:px-28 max-[400px]:text-sm max-[400px]:px-24" />
+                    <Button label={t('submit')} loading={loading} onClick={verifyEmail} className="bg-slate-900 text-white mt-10 font-poppins border border-none outline outline-none rounded py-2 px-36 max-[500px]:px-28 max-[400px]:text-sm max-[400px]:px-24" />
+                    <Dialog visible={emailInterface} modal onHide={() => { if (!emailInterface) return; setEmailInterface(false); }} className="w-[50%]">
+                        <img src={emailVerify} alt="Email verification" className="w-48 h-36 flex justify-center mx-auto items-center" />
+                        <div className="mt-6 px-12">
+                            <h3 className="font-kanit text-center text-lg mt-4">Veuillez vérifier votre boîte e-mail</h3>
+                            <p className="text-center text-sm font-poppins mt-4">Nous avons envoyé un email à votre adresse tix@gmail.com. Veuillez vérifier et continuer votre inscription à partir du lien envoyé</p>
+                        </div>
+
+                        <p className="text-center text-xs font-poppins mt-4 mb-6 cursor-pointer text-red-600 hover:text-red-700">Vous n'avez pas reçu de code ?</p>
+                    </Dialog>
 
                     <div className="relative mt-6">
                         <Divider className="w-[24rem] max-[500px]:w-[20rem] max-[400px]:w-[16rem]" />
@@ -66,7 +83,7 @@ const RegisterEmailUser = () => {
                     </div>
 
                     <div className="relative mt-4">
-                        <Button label={t('googleSignup')} className="font-poppins text-sm rounded border border-none outline outline-none px-24 max-[500px]:px-16 max-[400px]:px-10 max-[400px]:text-xs" icon="pi pi-google" onClick={verifyEmail} />
+                        <Button label={t('googleSignup')} className="font-poppins text-sm rounded border border-none outline outline-none px-24 max-[500px]:px-16 max-[400px]:px-10 max-[400px]:text-xs" icon="pi pi-google" />
                     </div>
 
                     <p className="text-xs mt-10 max-[400px]:text-[0.7em]">{t('accountExist')} <Link to="/register-email" className="font-semibold">{t('login')}</Link></p>
