611885191ba5d20fa65938fd1e0cda0c81aa6b28
jerry <jerry@mg-consulting-it-act.com>
2026-03-05 11:14:03 +0300

    configure docker for render

--- back/docker/start.sh
+++ back/docker/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Remplacer la ligne "listen" par $PORT
-sed -i "s/listen .*/listen ${PORT};/" /etc/nginx/conf.d/default.conf
+# Remplacer ${PORT} dans default.conf
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp && mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 
 # Permissions Laravel
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
-# Lancer PHP-FPM en arrière-plan
+# Démarrer PHP-FPM en arrière-plan
 php-fpm &
 
-# Lancer Nginx au premier plan
+# Démarrer Nginx au premier plan
 nginx -g "daemon off;"
