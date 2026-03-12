a80ddfa80f19e3f788893036490c4e383d746872
jerry <jerry@mg-consulting-it-act.com>
2026-03-12 20:20:03 +0300

    config cors for prod

--- back/config/cors.php
+++ back/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'auth/register', 'auth/login', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
