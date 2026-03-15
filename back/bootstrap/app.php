a7ff9395c9ff3abc1a6a78a729528e7b9ff28ba8
jerry <jerry@mg-consulting-it-act.com>
2026-03-15 17:16:48 +0300

    fix: remove unecessary cors

--- back/bootstrap/app.php
+++ back/bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
         // $middleware->append(\App\Http\Middleware\SetLocale::class);
     })
